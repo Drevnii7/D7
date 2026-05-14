e7aa1abc04cc68faa134f81e49279d24cee5cc24
Drevnii7 <Drevnii7@DESKTOP-D93AQ7O>
2026-05-14 19:09:01 +0500

    build 29: writed custom assert

--- D7/D7.cpp
+++ D7/D7.cpp
@@ -1,6 +1,9 @@
 ﻿#include <iostream>
+#include "source/utils/assert.hpp"
 
 int main()
 {
 	std::cout << "Hello from first rewrite from zero!\n";
+
+	assert(1 == 0, "test", "testtoken");
 }
---
+++ D7/source/utils/assert.hpp
@@ -0,0 +1,65 @@
+#pragma once
+#include <iostream> // std::cerr
+#include <cstdlib>  // std::abort
+
+/*
+    Custom assert
+    Work in DEBUG and RELEASE
+
+
+    Example work assert and unreachable:
+
+    - assert(1 == 0);
+    - assert(1 == 0, "test");
+    - assert(1 == 0, "test", "testtoken");
+
+    - unreachable();
+    - unreachable("test");
+    - unreachable("test", "testtoken");
+*/
+
+namespace d7
+{
+    namespace assert
+    {
+        inline void AssertImplementation
+        (
+            const char* expression,
+            const char* file,
+            int line,
+            const char* message = nullptr,
+            const char* dump_token = nullptr
+        )
+        {
+            std::cerr << "\n<=========ASSERTION FAILED==========\n";
+            std::cerr << "File: " << file << "\n";
+            std::cerr << "Line: " << line << "\n";
+            // Hide expression if call from unreachable
+            if (expression != "false")
+            {
+                std::cerr << "Expression: " << expression << "\n";
+            }
+            if (message != nullptr)
+            {
+                std::cerr << "Message: " << message << "\n";
+            }
+            if (dump_token != nullptr)
+            {
+                std::cerr << "Token: " << dump_token << "\n";
+            }
+            std::cerr << "==========ASSERTION FAILED=========>\n";
+        }
+    }
+}
+
+// Args: expression, message, token dump
+#define assert(expr, ...) \
+    do { \
+        if (!(expr)) { \
+            d7::assert::AssertImplementation(#expr, __FILE__, __LINE__, __VA_ARGS__); \
+            std::abort(); \
+        } \
+    } while(0)
+
+// Args: message, token dump
+#define unreachable(...) assert(false, __VA_ARGS__)
