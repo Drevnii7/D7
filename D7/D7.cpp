ec9e1209c9ff21a4fc382a66a9bc08945b96b2b7
Drevnii7 <Drevnii7@DESKTOP-D93AQ7O>
2026-05-14 17:26:08 +0500

    build 27: clear all files. Project rewrited from zero

--- D7/D7.cpp
+++ D7/D7.cpp
@@ -1,49 +1,6 @@
 ﻿#include <iostream>
 
-#include "source/lexer/Token.h"
-#include "source/lexer/CLexer.h"
-
-#include "source/preprocessor/IPreprocessor.h"
-#include "source/preprocessor/CStandartPreprocessor.h"
-
-#include "source/parser/AST.h"
-#include "source/parser/CParser.h"
-
-#include "source/codegenerator/CodeGenerator.h"
-
 int main()
 {
-    std::string File_SourceCode = "test_code.d7";
-
-    std::string File_AfterLexer = "test_code.lexer";
-    std::string File_AfterPreprocessor = "test_code.tokens";
-    std::string File_AfterParser = "test_code.ast";
-
-    std::string File_Out = "test_code.exe";
-
-    CLexer Lexer;
-    Lexer.LoadCode(File_SourceCode);
-    Lexer.RunProcessing();
-    Lexer.DebugPrint();
-    Lexer.SaveTokens(File_AfterLexer);
-
-    CStandartPreprocessor Preprocessor;
-    Preprocessor.LoadTokens(File_AfterLexer);
-    Preprocessor.RunProcessing();
-    Preprocessor.DebugPrint();
-    Preprocessor.SaveTokensAsCode(File_AfterPreprocessor);
-
-    CParser Parser;
-    Parser.LoadTokens(File_AfterPreprocessor);
-    Parser.RunProcessing();
-    Parser.DebugPrint();
-    Parser.SaveAST(File_AfterParser);
-
-    /*
-    ICodeGenerator CodeGenerator;
-    CodeGenerator.LoadAST(File_AfterParser);
-    CodeGenerator.RunProcessing();
-    CodeGenerator.DebugPrint();
-    CodeGenerator.SaveProgram(File_Out);
-    */
+	std::cout << "Hello from first rewrite from zero!\n";
 }
--- D7/source/BaseTypes.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#if __cplusplus >= 202002L || (defined(_MSVC_LANG) && _MSVC_LANG >= 202002L) || (defined(_HAS_CXX20) && _HAS_CXX20 == 1)
-	#define IS_CPP_20 1
-#else
-	#define IS_CPP_20 0
-#endif
